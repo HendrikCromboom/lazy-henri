0fdb343061c9830bccd60d83c2822f206345c7d5
HendrikCromboom <hcromboom@gmail.com>
2020-08-25 01:37:51 +0200

    lzArrMonths, lzArrDays, lzRanColor -> init

---
+++ lazy-henri.js
@@ -0,0 +1,17 @@
+// Arrays
+
+// Array of Months in Words
+var lzArrMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+// Array of Months in Words
+var lzArrDays = ['Monday', 'Tuesday', 'Wensday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+//Random
+
+//Random Color in HEX
+var RandColorHex = '0123456789ABCDEF';
+var RanColorHexCombined = '#';
+for (var i = 0; i < 6; i++) {
+    RanColorHexCombined += RandColorHex[Math.floor(Math.random() * 16)];}
+var lzRanColor = RanColorHexCombined
+
+
